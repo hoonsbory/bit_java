d9af5be7cdf6fb87f6fb53f5eeb3bd509a15f042
hoonsbory <tonymkcv@naver.com>
2021-03-07 22:36:02 +0900

    .

--- pcEtc/streamMode.js
+++ pcEtc/streamMode.js
@@ -1,12 +1,10 @@
 
 export default function streamMode(ua, streamTrue, camErr, video) {
-    alert(ua)
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         //사파리는 해상도가 자동으로 조절되기때문에 해상도를 설정해주면 에러가난다. 그래서 사파리는 설정을 안해줘야하는데,
         //기기 정보를 받아올때 크롬에는 사파리 크롬이 다 적혀있고, 사파리에는 사파리만 적혀있으므로 사파리를 특정하기 위해서는 
         //사파리 문자를 포함하고 크롬 문자를 포함하지않는 조건을 충족시켜줘야한다
         if (ua.indexOf('Safari') != -1 && ua.indexOf('Chrome') == -1) {
-            alert(1)
 
             navigator.mediaDevices.getUserMedia({
                 video: {
@@ -27,7 +25,6 @@ export default function streamMode(ua, streamTrue, camErr, video) {
             });
         } else {
             // Not adding `{ audio: true }` since we only want video now
-            alert(2)
 
             navigator.mediaDevices.getUserMedia({
                 video: {
@@ -47,12 +44,11 @@ export default function streamMode(ua, streamTrue, camErr, video) {
         }
     }
     else if (navigator.getUserMedia) { // Standard
-        alert(3)
 
         navigator.getUserMedia({
             video: {
                 width: { min: 0, ideal: 1280, max: 1920 },
-                height: { min: 0, ideal: 720, max: 1080 }, facingMode: { exact: "user" }
+                height: { min: 0, ideal: 1280, max: 1920 }, facingMode: { exact: "user" }
             }
         }, function (stream) {
             streamTrue();
@@ -60,12 +56,11 @@ export default function streamMode(ua, streamTrue, camErr, video) {
             video.play();
         }, camErr());
     } else if (navigator.webkitGetUserMedia) { // WebKit-prefixed
-        alert(4)
 
         navigator.webkitGetUserMedia({
             video: {
                 width: { min: 0, ideal: 1280, max: 1920 },
-                height: { min: 0, ideal: 720, max: 1080 }, facingMode: { exact: "user" }
+                height: { min: 0, ideal: 1280, max: 1920 }, facingMode: { exact: "user" }
             }
         }, function (stream) {
             streamTrue();
@@ -73,12 +68,11 @@ export default function streamMode(ua, streamTrue, camErr, video) {
             video.play();
         }, camErr());
     } else if (navigator.mozGetUserMedia) { // Mozilla-prefixed
-        alert(5)
 
         navigator.mozGetUserMedia({
             video: {
                 width: { min: 0, ideal: 1280, max: 1920 },
-                height: { min: 0, ideal: 720, max: 1080 }, facingMode: { exact: "user" }
+                height: { min: 0, ideal: 1280, max: 1920 }, facingMode: { exact: "user" }
             }
         }, function (stream) {
             streamTrue();
