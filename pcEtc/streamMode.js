bf8c1e26dfa9dddfded09f45cd3807976c3db6fe
hoonsbory <tonymkcv@naver.com>
2021-10-04 19:05:36 +0900

    .

--- pcEtc/streamMode.js
+++ pcEtc/streamMode.js
@@ -30,8 +30,8 @@ export default function streamMode(ua, streamTrue, camErr, video) {
       navigator.mediaDevices
         .getUserMedia({
           video: {
-            width: { min: 0, ideal: 1280, max: 2560 },
-            height: { min: 0, ideal: 894, max: 1788 },
+            height: { min: 0, ideal: 1280, max: 2560 },
+            width: { min: 0, ideal: 894, max: 1788 },
             facingMode: "user",
           },
         })
